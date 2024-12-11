bec4f64bfa5af368cb38691227a53b186de34e9b
Nikita <91425615+Nikita-Dmytriienko@users.noreply.github.com>
2024-12-11 15:37:33 +0200

    added some logic(controller)

--- Models/Joke.cs
+++ Models/Joke.cs
@@ -5,6 +5,9 @@
         public int Id { get; set; }
         public string JokeQuestion { get; set; }
         public string JokeAnswer { get; set; }
+        public string Category {  get; set; }
+        public int Rating { get; set; }
+        public DateTime CreatedDate { get; set; }
         public Joke()
         {
           
