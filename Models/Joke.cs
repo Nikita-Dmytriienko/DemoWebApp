89c2de841d00957db761f5d28313b26c5cc0fa31
Nikita <91425615+Nikita-Dmytriienko@users.noreply.github.com>
2024-12-11 16:53:52 +0200

    refac

--- Models/Joke.cs
+++ Models/Joke.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string JokeQuestion { get; set; }
         public string JokeAnswer { get; set; }
-   /*   public string Category {  get; set; }
+     /*   public string Category {  get; set; }
         public int Rating { get; set; }
         public DateTime CreatedDate { get; set; }*/
         public Joke()
